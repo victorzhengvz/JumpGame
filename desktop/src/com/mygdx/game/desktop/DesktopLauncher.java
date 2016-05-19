80547eb0fdffb083f84131c8d55bee57aaaa969d
Victor Zheng <Victor.Zheng.1@stonybrook.edu>
2016-05-19 18:38:23 -0400

    Initial commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyJumpGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Jump Game";
+		config.width = 480;
+		config.height = 320;
+		new LwjglApplication(new MyJumpGame(), config);
+	}
+}
